952cfdc0e706a47035f4e9e1b0476a9b0f96e4c0
mejxe <mlaskowski707@gmail.com>
2024-12-13 09:56:59 +0100

    Add gauss elimination 0.1

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -1,13 +1,29 @@
 #include "gauss.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+     	 for(int i=1;i<=b->r-1;i++) {
+
+             if(mat->data[i][i] == 0.0)
+             {
+                 printf("Mathematical Error!");
+                 exit(0);
+             }
+             for(int j=i+1;j<=b->r;j++)
+             {
+                 double ratio = mat->data[j][i]/b->data[i][i];
+
+                 for(int k=1;k<=b->r+1;k++)
+                 {
+                     mat->data[j][k] = mat->data[j][k] - ratio*mat->data[i][k];
+                 }
+		  }
+	 }
 
 		return 0;
 }
